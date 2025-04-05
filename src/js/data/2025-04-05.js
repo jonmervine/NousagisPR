f1c071e67281b02b26a0ff4d77564efed9564697
DarkMage530 <jon.mervine@gmail.com>
2025-04-05 17:52:14 -0400

    Update 2025-04-05.js

--- src/js/data/2025-04-05.js
+++ src/js/data/2025-04-05.js
@@ -1,4 +1,195 @@
-const VIDEOSOURCE = {
-    YOUTUBE: 'YOUTUBE',
-    CATBOX: 'CATBOX'
-}
+dataSetVersion = "2025-04-05"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+    {
+        name: "Filter by type of songs",
+        key: "singer",
+        tooltip: "Check this to show songs",
+        checked: true,
+        sub: [
+            {name: "INS", key: "INS", checked: true},
+            {name: "OP", key: "OP", checked: true},
+            {name: "ED", key: "ED", checked: true}
+        ]
+    }
+];
+
+dataSet[dataSetVersion].characterData = [
+    {
+        name: "Bohemian Rhapsody",
+        videoId: "fJ9rUzIMcZQ",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Queen"]
+        }
+    },
+    {
+        name: "Migraine",
+        videoId: "pjvKYnBBEQM",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["twenty one pilots"]
+        }
+    },
+    {
+        name: "Have you ever seen the rain",
+        videoId: "bO28lB1uwp4",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["creedence clearwater revival"]
+        }
+    },
+    {
+        name: "Free bird",
+        videoId: "0LwcvjNJTuM",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["lynyrd skynyrd"]
+        }
+    },
+    {
+        name: "Clocks",
+        videoId: "d020hcWA_Wg",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Coldplay"]
+        }
+    },
+    {
+        name: "Sail",
+        videoId: "tgIqecROs5M",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["AWOLNATION"]
+        }
+    },
+    {
+        name: "Get your wish",
+        videoId: "4SZEDBFPpgw",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Porter Robinson"]
+        }
+    },
+    {
+        name: "King Kunta",
+        videoId: "hRK7PVJFbS8",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Kendrick Lamar"]
+        }
+    },
+    {
+        name: "Golden god",
+        videoId: "OKV0M2lEm-s",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Yung Lean", "Bladee"]
+        }
+    },
+    {
+        name: "Iron lung",
+        videoId: "Njk2YAgNMnE",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["King Gizzard & The Lizard Wizard"]
+        }
+    },
+    {
+        name: "This modern love",
+        videoId: "oZ9vPoecPZs",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Bloc Party"]
+        }
+    },
+    {
+        name: "Brittle bones Nicky",
+        videoId: "Gtv40bOE3U4",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Rare Americans"]
+        }
+    },
+    {
+        name: "Playing god",
+        videoId: "iDy2wCQYSrU",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Paramore"]
+        }
+    },
+    {
+        name: "Titanium",
+        videoId: "JRfuAukYTKg",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["David Guetta", "Sia"]
+        }
+    },
+    {
+        name: "Battle symphony",
+        videoId: "D7ab595h0AU",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Linkin Park"]
+        }
+    },
+    {
+        name: "Thnks fr th mmrs",
+        videoId: "cMY5VwfcGdU",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Fall out boys"]
+        }
+    },
+    {
+        name: "iSpy",
+        videoId: "5XK4v2fgMPU",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["KYLE & Lil Yachty"]
+        }
+    },
+    {
+        name: "See you again (alt ver)",
+        videoId: "sODAN0XvRfw",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Tyler the Creator"]
+        }
+    },
+    {
+        name: "All along the watchtower",
+        videoId: "TLV4_xaYynY",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Jimi Hendrix"]
+        }
+    },
+    {
+        name: "Gun",
+        videoId: "ktoaj1IpTbw",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["CHVRCHES"]
+        }
+    },
+    {
+        name: "Heaven",
+        videoId: "8RVWepy4Ay4",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["Isyana Sarasvati", "Afgan", "Rendy Pandugo"]
+        }
+    },
+    {
+        name: "Robot stop",
+        videoId: "9p_Si21ig7c",
+        videoSource: VIDEOSOURCE.YOUTUBE,
+        opts: {
+            singer: ["King Gizzard & The Lizard Wizard"]
+        }
+    }
+];
