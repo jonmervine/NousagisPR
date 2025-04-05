50228585f0f0865c4b97f43dba528741872528c9
DarkMage530 <jon.mervine@gmail.com>
2025-04-05 18:24:40 -0400

    Update 2025-04-05.js

--- src/js/data/2025-04-05.js
+++ src/js/data/2025-04-05.js
@@ -6,12 +6,7 @@ dataSet[dataSetVersion].options = [
         name: "Filter by type of songs",
         key: "singer",
         tooltip: "Check this to show songs",
-        checked: true,
-        sub: [
-            {name: "INS", key: "INS", checked: true},
-            {name: "OP", key: "OP", checked: true},
-            {name: "ED", key: "ED", checked: true}
-        ]
+        checked: true
     }
 ];
 
